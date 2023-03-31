ed0d2600a84dbc787d3d0c59ad5078bb677d62bb
Maycol Perez <maycoldanielp@gmail.com>
2023-03-30 20:53:38 -0400

    app finished

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,40 +1,66 @@
 const Book = require('../models/Book');
 const Category = require('../models/Category');
+const Author = require('../models/Author');
+const Publisher = require('../models/Publisher');
 
 
 exports.getHome = async (req, res) => {
 
     const categories = await Category.findAll({raw: true});
 
-    Book.findAll({raw: true}).then(result => {
-        res.status(200).render('home', {
-            pageName: 'Home',
-            books: result,
-            hasBook: result.length > 0,
-            categories
-        });
-
-    }).catch(err => {
-        console.log(err);
+    const ListBooks = await Book.findAll({
+        include: [{ model: Category }, { model: Author }, { model: Publisher }],
+        raw: true
     });
+
+    const list =  ListBooks.map( (p) => {
+        const {id, title, yearReleased, imagePath} = p;
+        const author = p['Author.name'];
+        const category = p['Category.name'];
+        const publisher = p['Publisher.name']; 
+
+        return {id, title, yearReleased, imagePath, author, category, publisher};
+    });
+
+    res.status(200).render('home', {
+        pageName: 'Home',
+        books: list,
+        hasBook: list.length > 0,
+        categories
+    });
+    
 };
 
 
-exports.bookFind = (req, res) => {
+exports.bookFind = async (req, res) => {
     const bookName = req.body.bookName;
+    
     if (!bookName) {
         return res.redirect('/');
     }
+    const categories = await Category.findAll({raw: true});
 
-    Book.findAll({where: {title: bookName}}).then(result => {   
-        res.status(200).render('home', {
-            pageName: 'Home',
-            books: result.map(p => p.dataValues),
-            hasBook: result.length > 0
-        });
+    const ListBooks = await Book.findAll({
+        include: [{ model: Category }, { model: Author }, { model: Publisher }],
+        raw: true,
+        where: {title: bookName}
+    });
 
-    }).catch(err => {
-        console.log(err);
+    const list =  ListBooks.map( (p) => {
+        const {id, title, yearReleased, imagePath} = p;
+        const author = p['Author.name'];
+        const category = p['Category.name'];
+        const publisher = p['Publisher.name']; 
+
+        return {id, title, yearReleased, imagePath, author, category, publisher};
+    });
+
+
+    res.status(200).render('home', {
+        pageName: 'Home',
+        books: list,
+        hasBook: list.length > 0,
+        categories
     });
 
 };
@@ -43,23 +69,50 @@ exports.filterBooks = async (req, res) => {
     
     const categoriesSelected = req.body.categoryFilter;
     const bookList = [];
-    
+
+    if (!categoriesSelected) {
+        return res.status(400).redirect('/');
+    }
+
     if (categoriesSelected.length < 2) {
-      
-        const list = await Book.findAll( {raw: true, where: {CategoryId: categoriesSelected}} );
-       
-        list.forEach(e => bookList.push(e) );
+
+        const list = await Book.findAll({
+            include: [{ model: Category }, { model: Author }, { model: Publisher }],
+            raw: true,
+            where: {CategoryId: categoriesSelected}
+        });
+
+        const listMapped =  list.map( (p) => {
+            const {id, title, yearReleased, imagePath} = p;
+            const author = p['Author.name'];
+            const category = p['Category.name'];
+            const publisher = p['Publisher.name']; 
+    
+            return {id, title, yearReleased, imagePath, author, category, publisher};
+        });
+
+        listMapped.forEach(e => bookList.push(e) );
 
     } else {
 
         for (const id of categoriesSelected) {
-                
+            
             const list = await Book.findAll({
+                include: [{ model: Category }, { model: Author }, { model: Publisher }],
                 raw: true,
                 where: {CategoryId: id}
+            });  
+
+            const listMapped =  list.map( (p) => {
+                const {id, title, yearReleased, imagePath} = p;
+                const author = p['Author.name'];
+                const category = p['Category.name'];
+                const publisher = p['Publisher.name']; 
+        
+                return {id, title, yearReleased, imagePath, author, category, publisher};
             });
 
-            list.forEach(e => bookList.push(e) );
+            listMapped.forEach(e => bookList.push(e) );
         }
     }
     const categories = await Category.findAll({raw: true});
